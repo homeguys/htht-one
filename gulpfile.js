0f0cad3eb35f70b83a3bc1796284e18c4922da78
wallet <918061733@qq.com>
2020-01-10 18:11:43 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const { src, dest } = require("gulp");
 const babel = require("gulp-babel");
 const sass = require("gulp-sass");
 const gulpif = require("gulp-if");
+const autoprefixer = require("gulp-autoprefixer");
+// const uglify = require("gulp-uglify");
+// const cleanCSS = require("gulp-clean-css");
 
 function isJavaScript(file) {
   // 判断文件的扩展名是否是 '.jsx'
@@ -9,14 +12,30 @@ function isJavaScript(file) {
 }
 
 function isCss(file) {
-  // 判断文件的扩展名是否是 '.jsx'
-  return file.extname === ".scss";
+  // 判断文件的扩展名是否是css文件
+  const cssRegex = /\.(scss|sass|css)$/;
+  return cssRegex.test(file.extname);
 }
 
 // eslint-disable-next-line func-names
 exports.default = function() {
-  return src(["src/**/demo/index.jsx", "src/**/style/*"])
-    .pipe(gulpif(isJavaScript, babel()))
-    .pipe(gulpif(isCss, sass()))
-    .pipe(dest("lib/"));
+  return (
+    src([
+      "src/components/**/demo/index.jsx",
+      "src/components/**/utils/*.jsx",
+      "src/components/*.jsx",
+      "src/components/**/style/*"
+    ])
+      .pipe(gulpif(isJavaScript, babel()))
+      // .pipe(gulpif(isJavaScript, uglify()))
+      .pipe(
+        gulpif(
+          isCss,
+          sass.sync({ outputStyle: "expanded" }).on("error", sass.logError)
+        )
+      )
+      .pipe(gulpif(isCss, autoprefixer()))
+      // .pipe(gulpif(isCss, cleanCSS({ compatibility: "ie8" })))
+      .pipe(dest("lib/"))
+  );
 };
